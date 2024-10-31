919405190426f0ee58e6b9a63d39585a2653279b
João Augusto Palma Paixão <joaoaugustopp@gmail.com>
2024-10-31 11:05:48 -0300

    criei o ex011 na aula 012

---
+++ exercicios/aula12/ex011.js
@@ -0,0 +1,10 @@
+var names = window.prompt('Digite seu nome: ')
+var age = window.prompt('Digite sua idade: ')
+
+if (age < 16) {
+    window.alert(`${names} tem ${age} anos, então você NÃO vota!`)
+} else if (age < 18 || age > 65) {
+    window.alert(`${names} tem ${age} anos, então seu voto é OPCIONAL!`)
+} else {
+    window.alert(`${names} tem ${age} anos, então você é OBRIGADO a votar!`)
+}
