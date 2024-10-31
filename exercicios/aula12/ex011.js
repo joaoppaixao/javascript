4a053b4482227103eb60fda50c1957c745bb5e58
João Augusto Palma Paixão <joaoaugustopp@gmail.com>
2024-10-31 11:41:12 -0300

    atualizei ex011

--- exercicios/aula12/ex011.js
+++ exercicios/aula12/ex011.js
@@ -3,7 +3,7 @@ var age = window.prompt('Digite sua idade: ')
 
 if (age < 16) {
     window.alert(`${names} tem ${age} anos, então você NÃO vota!`)
-} else if (age < 18 || age > 65) {
+} else if (age < 18 || age >= 65) {
     window.alert(`${names} tem ${age} anos, então seu voto é OPCIONAL!`)
 } else {
     window.alert(`${names} tem ${age} anos, então você é OBRIGADO a votar!`)
