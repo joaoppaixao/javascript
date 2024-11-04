483a98bcf76d32677fdd917620d016a7271a0fac
João Augusto Palma Paixão <joaoaugustopp@gmail.com>
2024-11-04 19:44:22 -0300

    finalizei o ex013

--- exercicios-condiçoes/ex013/script.js
+++ exercicios-condiçoes/ex013/script.js
@@ -2,13 +2,12 @@ function calcular() {
     var n1 = Number(window.prompt('Digite um número: '))
     var n2 = Number(window.prompt('Digite outro número: '))
 
-    var maior
-    if (n1 > n2) {
-        maior = `${n1}`
-    } else {
-        maior = `${n2}`
-    }
-
     var res = document.getElementById('res')
-    msg.innerHTML = `<p>Analisando os valores ${n1} e ${n2}, o maior valor é ${maior}.</p>`
+    if (n1 > n2) {
+        res.innerHTML = `<p>Analisando os valores <mark>${n1}</mark> e <mark>${n2}</mark>, o maior valor é <mark>${n1}</mark>.`
+    } else if(n1 < n2) {
+        res.innerHTML = `<p>Analisando os valores <mark>${n1}</mark> e <mark>${n2}</mark>, o maior valor é <mark>${n2}</mark>.`
+    } else {
+        res.innerHTML = `<p>Analisando os valores <mark>${n1}</mark> e <mark>${n2}</mark>, ambos são <mark>IGUAIS</mark>.`
+    }
 }
