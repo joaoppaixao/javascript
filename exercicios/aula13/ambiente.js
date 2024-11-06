603b5634cb544451947a7b35998d425b685337bd
João Augusto Palma Paixão <joaoaugustopp@gmail.com>
2024-11-06 14:28:05 -0300

    criei aula14

--- exercicios/aula13/ambiente.js
+++ exercicios/aula13/ambiente.js
@@ -1,5 +1,10 @@
 var c = 1
 while (c < 6) {
-    console.log('Tudo bem?')
+    console.log(`Passo ${c}`)
     c++
 }
+
+do {
+    console.log(`Passo ${c}`)
+    c++
+} while (c < 6)
---
+++ exercicios/aula14/ambiente.js
@@ -0,0 +1,5 @@
+console.log('Vai começar...')
+for (var c = 1; c <= 10; c++) {
+    console.log(`Exemplo ${c}`)
+}
+console.log('FIM!')
