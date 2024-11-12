9316a03af4902a9668390006093afa64807e1fa5
João Augusto Palma Paixão <joaoaugustopp@gmail.com>
2024-11-12 10:27:05 -0300

    criei ex027

---
+++ exercicios-repetiçoes/ex027/script.js
@@ -0,0 +1,9 @@
+function tabuada() {
+    var num = Number(document.getElementById('txtn').value)
+    var res = document.getElementById('saida')
+
+    res.innerHTML += `<h2>Tabuada do ${num}</h2>`
+    for (var i = 1; i <= 10; i++) {
+        res.innerHTML += `${num} x ${i} = <strong>${num * i}</strong><br>`
+    }
+}
